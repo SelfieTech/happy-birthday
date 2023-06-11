c11ce361731b2735b55efcefeedc1e79fdfc8497
Hemant Tolani <67514928+SelfieTech@users.noreply.github.com>
2023-06-11 22:37:11 +0530

    Update effect.js

--- effect.js
+++ effect.js
@@ -123,7 +123,7 @@ $('document').ready(function(){
 	$('#balloons_flying').click(function(){
 		$('.balloon-border').animate({top:-500},8000);
 		$('#b1,#b4,#b5,#b7,#b9').addClass('balloons-rotate-behaviour-one');
-		$('#b2,#b3,#b6,#b8').addClass('balloons-rotate-behaviour-two');
+		$('#b2,#b3,#b6,#b8,#b9').addClass('balloons-rotate-behaviour-two');
 		// $('#b3').addClass('balloons-rotate-behaviour-two');
 		// $('#b4').addClass('balloons-rotate-behaviour-one');
 		// $('#b5').addClass('balloons-rotate-behaviour-one');
